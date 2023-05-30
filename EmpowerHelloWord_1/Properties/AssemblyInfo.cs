7de21bc514cb1c47f599c76a276d9bf673b53709
mylevangelista <mylena.evangelista@g.globo>
2023-05-30 15:55:41 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerHelloWord_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWord_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWord_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D8C8D92B-B31E-4817-BF6B-F4DF125911EA")]
+[assembly: AssemblyVersion("1.0.0.0")]
